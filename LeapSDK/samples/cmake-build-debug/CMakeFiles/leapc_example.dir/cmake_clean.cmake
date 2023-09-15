151a08d71c750660f565e1fc9cdac730ba52e986
Ron <tahmeat@meatsmotel.com>
2023-09-15 11:01:14 -0500

    Ron Branch Init: LeapSDK pushed.

---
+++ LeapSDK/samples/cmake-build-debug/CMakeFiles/leapc_example.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leapc_example.dir/leapc_main.c.obj"
+  "CMakeFiles/leapc_example.dir/leapc_main.c.obj.d"
+  "leapc_example.exe"
+  "leapc_example.exe.manifest"
+  "leapc_example.pdb"
+  "libleapc_example.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/leapc_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
