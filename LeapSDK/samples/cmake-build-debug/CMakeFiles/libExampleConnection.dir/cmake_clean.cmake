fc81a3322ab6f13ef014d232d9e017b3eeca225d
Ron <tahmeat@meatsmotel.com>
2023-09-22 11:50:03 -0500

    Delete LeapSDK directory

--- LeapSDK/samples/cmake-build-debug/CMakeFiles/libExampleConnection.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/libExampleConnection.dir/ExampleConnection.c.obj"
-  "CMakeFiles/libExampleConnection.dir/ExampleConnection.c.obj.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/libExampleConnection.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
