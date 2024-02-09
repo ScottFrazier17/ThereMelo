6819c5c65bd67b0046a0e6f9817b9e431919eb52
TahMeat <ron.nguyen@mavs.uta.edu>
2024-02-08 20:52:53 -0600

    wowza.

--- Unity - Theremin/Assets/Scripts/Audio/AudioManager.cs
+++ Unity - Theremin/Assets/Scripts/Audio/AudioManager.cs
@@ -2,20 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
+using System.Runtime.InteropServices;
+using System;
+using FMOD;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance { get; private set; }
 
+    private FMOD.System system;
+    private FMOD.DSP fftDsp;
+    private FMOD.ChannelGroup channelGroup;
+
     private void Awake()
     {
         if (instance != null)
         {
-            Debug.LogError("Multiple Audio Testers in Scene.");
+            UnityEngine.Debug.LogError("Multiple Audio Testers in Scene.");
         }
         instance = this;
-    }
 
+        // start fft dsp.
+        RuntimeManager.CoreSystem.getMasterChannelGroup(out channelGroup);
+        RuntimeManager.CoreSystem.createDSPByType(FMOD.DSP_TYPE.FFT, out fftDsp);
+        fftDsp.setParameterInt((int)FMOD.DSP_FFT.WINDOWTYPE, (int)FMOD.DSP_FFT_WINDOW.HANNING);
+        fftDsp.setParameterInt((int)FMOD.DSP_FFT.WINDOWSIZE, 1024 * 2); // Window size for FFT
+        channelGroup.addDSP(FMOD.CHANNELCONTROL_DSP_INDEX.HEAD, fftDsp);
+
+        fftDsp.setActive(true);
+    }
+    void Update()
+    {
+        // fft dataa
+        IntPtr unmanagedData;
+        uint length;
+        fftDsp.getParameterData((int)FMOD.DSP_FFT.SPECTRUMDATA, out unmanagedData, out length);
+        FMOD.DSP_PARAMETER_FFT fft = (FMOD.DSP_PARAMETER_FFT)System.Runtime.InteropServices.Marshal.PtrToStructure(unmanagedData, typeof(FMOD.DSP_PARAMETER_FFT));
+
+        if (fft.numchannels > 0)
+        {
+            // analyze channel.
+            float maxMagnitude = 0f;
+            int maxIndex = 0;
+            for (int i = 0; i < fft.length; i++)
+            {
+                float magnitude = fft.spectrum[0][i];
+                if (magnitude > maxMagnitude)
+                {
+                    maxMagnitude = magnitude;
+                    maxIndex = i;
+                }
+            }
+            int sampleRate;
+            RuntimeManager.CoreSystem.getSoftwareFormat(out sampleRate, out _, out _);
+            float frequency = maxIndex * sampleRate / (float)fft.length;
+            UnityEngine.Debug.Log("Detected Frequency: " + frequency + " Hz");
+
+            // convert frequency to musical note later
+        }
+    }
     public void PlayOneShot(EventReference sound, Vector3 worldPos)
     {
         // plays audio once
